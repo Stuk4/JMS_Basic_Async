6fd6d6b1a28f6157d50ea6c04e98211a637aba4d
Carlos Larico <cdaniel.lf@gmail.com>
2016-05-21 12:06:58 -0500

    Preparing constants

---
+++ README.md
@@ -0,0 +1,7 @@
+## Queue List
+Type: javax.jms/Queue
+|JNDI NAME   |  Physical Destination Name |
+|--:|--:|
+|jms/QueueIn   |   QueueIn|
+|jms/QueueOut   |QueueOut|
+
---
+++ src/main/java/edu/cibertec/jaad/jms/JMSClient.java
@@ -0,0 +1,92 @@
+package edu.cibertec.jaad.jms;
+
+import org.apache.log4j.Logger;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ * @author Carlos Larico [Stuk4] cdaniel.lf@gmail.com
+ */
+public class JMSClient {
+
+    public static final Logger LOG = Logger.getLogger(JMSClient.class);
+    public static final long WAITING_MSG = 601;
+
+    public static final String JMS_CONN_FAC = "jms/QUEUECF";
+    public static final String JMS_QUEUE_IN = "jms/QueueIn";
+    public static final String JMS_QUEUE_OUT = "jms/QueueOut";
+
+    private ConnectionFactory factory;
+    private Connection connection;
+    private Session session;
+    private Destination queueIn;
+    private Destination queueOut;
+    private Context ctx;
+
+    private MessageProducer producer;
+    private MessageConsumer consumer;
+
+    /**
+     * jms lookUp
+     */
+    public void jmsLookUp(){
+        try {
+            ctx = new InitialContext();
+            factory = (ConnectionFactory) ctx.lookup(JMS_CONN_FAC);
+            connection = factory.createConnection();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        } catch (NamingException|JMSException e) {
+            LOG.error("Error loading context ", e);
+        }
+
+    }
+
+    /**
+     * create destination
+     */
+    private void createDestination(){
+        try {
+            queueIn = (Destination) ctx.lookup(JMS_QUEUE_IN);
+            queueOut = (Destination) ctx.lookup(JMS_QUEUE_OUT);
+            connection.start();
+        } catch (NamingException|JMSException e) {
+            LOG.error("Error create destination", e);
+        }
+    }
+
+    /**
+     * close resources
+     */
+    private void closeResources(){
+        try {
+            producer.close();
+            consumer.close();
+            session.close();
+            connection.close();
+        } catch (JMSException e) {
+            e.printStackTrace();
+            LOG.error("Error closing resources", e);
+        }
+        //FIXME delete me
+        System.exit(0);
+    }
+
+    /**
+     * main
+     * @param args params
+     */
+    public static void main(String[] args) {
+        try {
+
+
+        } catch (Exception e) {
+            LOG.error("Error procesando el mensaje ",e);
+        }
+    }
+
+
+
+}
